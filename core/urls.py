8e314779cb024b02fcbd7f23412a5c792412e91d
sergey@telminov.ru <sergey@telminov.ru>
2016-09-03 12:22:31 +0300

    добавил редирект на doc

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^send/$', views.send),
     url(r'^get_state/$', views.get_state),
 ]
